b5443a99d109f7f30aa14438520c209909b3b9c7
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:00:39 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,11 +2,8 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Todo List',
-  icon: 'tabler:list-check',
   routes: {
     '/': lazy(() => import('@'))
   },
   hasAI: true,
-  category: 'Productivity'
 } satisfies ModuleConfig
