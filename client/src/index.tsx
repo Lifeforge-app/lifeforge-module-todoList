c305e48664e255d0edfbb07953d3e6a0d079ee9d
melvinchia3636 <melvinchia623600@gmail.com>
2026-06-09 19:00:16 +0800

    refactor: remove redundant css files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,8 +3,6 @@ import { ModuleHeader } from '@lifeforge/ui'
 import { TodoListProvider } from '@/providers/TodoListProvider'
 
 import TodoListContainer from './components/TodoListContainer'
-import './index.css'
-
 function TodoList() {
   return (
     <>
